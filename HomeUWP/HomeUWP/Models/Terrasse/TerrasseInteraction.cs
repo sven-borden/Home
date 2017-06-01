b8f2fdd08f33e4a63b194059c4457fcd1f69deed
Sven <glaglasven@live.com>
2017-06-01 10:26:35 +0200

    #1

---
+++ HomeUWP/HomeUWP/Models/Terrasse/TerrasseInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeUWP.Models.Terrasse
+{
+	public class TerrasseInteraction
+	{
+		private string ip;
+		private string port;
+
+		public TerrasseInteraction() : this("192.168.1.100", "80")
+		{
+
+		}
+
+		public TerrasseInteraction(string _ip) : this(_ip, "80")
+		{
+
+		}
+
+		public TerrasseInteraction(string _ip, string _port)
+		{
+			ip = _ip;
+			port = _port;
+		}
+	}
+}
