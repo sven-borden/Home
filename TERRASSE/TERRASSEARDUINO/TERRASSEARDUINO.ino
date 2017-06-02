224cebd20fe3b57915d6fe20c8a4dda12bb8c0ba
dxs <glaglasven@live.com>
2017-06-02 09:41:05 +0200

    Update arduino OK

--- TERRASSE/TERRASSEARDUINO/TERRASSEARDUINO.ino
+++ TERRASSE/TERRASSEARDUINO/TERRASSEARDUINO.ino
@@ -1,16 +1,24 @@
-#include <SotwareSerial.h>
-
-#define DEBUG
+#include <SoftwareSerial.h>
+#define DEBUG true
+#define REDPIN 9
+#define GREENPIN 6
+#define BLUEPIN 5
 
 SoftwareSerial esp(11,10);
 
 void setup()
 {
     Serial.begin(9600);
-    esp.begin(115200);
-
-    sendData("AT+RST\r\n",2000);
-    sendData("AT+CWMODE=2\r\n",1000);
+    //esp.begin(115200);
+    //sendData("AT+UART_DEF=9600,8,1,0,0",1000);
+    esp.begin(9600);
+  pinMode(REDPIN,OUTPUT);
+  pinMode(GREENPIN,OUTPUT);
+  pinMode(BLUEPIN,OUTPUT);
+  analogWrite(REDPIN,255); 
+    sendData("AT+CWMODE=1\r\n",1000);
+    sendData("AT+CWLAP\r\n",5000);
+    sendData("AT+CWJAP=\"P600\",\"Y0urn0ta110weD\"\r\n",10000);
     sendData("AT+CIFSR\r\n",1000); // get ip address
     sendData("AT+CIPMUX=1\r\n",1000); // configure for multiple connections
     sendData("AT+CIPSERVER=1,80\r\n",1000); // turn on server on port 80
@@ -32,17 +40,24 @@ void loop()
             int G = ConvertHexaToLum(esp.read(),esp.read());
 
             esp.find("blue=");
-            int B = ConvertHexaToLum(espread(),esp.read());
-            Serial.println(G);
-
+            int B = ConvertHexaToLum(esp.read(),esp.read());
+            //Serial.println(B);
             // make close command
             String closeCommand = "AT+CIPCLOSE="; 
             closeCommand+=connectionId; // append connection id
             closeCommand+="\r\n";
-            
-            sendData(closeCommand,1000,DEBUG); // close connection
+            sendHTTPResponse(connectionId,"RED GREEN BLUE OK");
+            SetLed(R,G,B);
+            //sendData(closeCommand,1000); // close connection
         }
     }
+}
+
+void SetLed(int R, int G, int B)
+{
+  analogWrite(REDPIN,R);
+  analogWrite(GREENPIN,G);
+  analogWrite(BLUEPIN,B);
 }
 
 int ConvertHexaToLum(int high, int low)
@@ -64,29 +79,84 @@ int ConvertHexaToLum(int high, int low)
     {
         value += (low-65);
     }
+    return value;
 }
 
 
-String sendData(String command, const int timeout, boolean debug)
+String sendData(String command, const int timeout)
 {
     String response = "";
     
-    esp8266.print(command); // send the read character to the esp8266
+    esp.print(command); // send the read character to the esp8266
     
     long int time = millis();
     
     while( (time+timeout) > millis())
     {
-      while(esp8266.available())
+      while(esp.available())
       {
         
         // The esp has data so display its output to the serial window 
-        char c = esp8266.read(); // read the next character.
+        char c = esp.read(); // read the next character.
         response+=c;
       }  
     }
     
-    if(debug)
+    if(true)
+    {
+      Serial.print(response);
+    }
+    
+    return response;
+}
+
+void sendHTTPResponse(int connectionId, String content)
+{
+     
+     // build HTTP response
+     String httpResponse;
+     String httpHeader;
+     // HTTP Header
+     httpHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n"; 
+     httpHeader += "Content-Length: ";
+     httpHeader += content.length();
+     httpHeader += "\r\n";
+     httpHeader +="Connection: close\r\n\r\n";
+     httpResponse = httpHeader + content + " "; // There is a bug in this code: the last character of "content" is not sent, I cheated by adding this extra space
+     sendCIPData(connectionId,httpResponse);
+}
+
+void sendCIPData(int connectionId, String data)
+{
+   String cipSend = "AT+CIPSEND=";
+   cipSend += connectionId;
+   cipSend += ",";
+   cipSend +=data.length();
+   cipSend +="\r\n";
+   sendCommand(cipSend,1000);
+   sendData(data,1000);
+}
+
+String sendCommand(String command, const int timeout)
+{
+    String response = "";
+           
+    esp.print(command); // send the read character to the esp8266
+    
+    long int time = millis();
+    
+    while( (time+timeout) > millis())
+    {
+      while(esp.available())
+      {
+        
+        // The esp has data so display its output to the serial window 
+        char c = esp.read(); // read the next character.
+        response+=c;
+      }  
+    }
+    
+    if(true)
     {
       Serial.print(response);
     }
