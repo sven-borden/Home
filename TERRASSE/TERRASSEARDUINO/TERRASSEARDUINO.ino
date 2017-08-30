103ce22c37b8bc4cf328baf740b0d3bdce41e2ab
dxs <glaglasven@live.com>
2017-08-30 22:26:41 +0200

    Update du fichier arduino working

--- TERRASSE/TERRASSEARDUINO/TERRASSEARDUINO.ino
+++ TERRASSE/TERRASSEARDUINO/TERRASSEARDUINO.ino
@@ -1,76 +1,173 @@
-#include <SotwareSerial.h>
+#include <SoftwareSerial.h>
+#define DEBUG  true
+ 
+#define REDPIN 5
+#define GREENPIN 6
+#define BLUEPIN 3
 
-#define DEBUG
+SoftwareSerial esp8266(8,9);
 
-SoftwareSerial esp(11,10);
+
 
 void setup()
 {
-    Serial.begin(9600);
-    esp.begin(115200);
-
-    sendData("AT+RST\r\n",2000);
-    sendData("AT+CWMODE=2\r\n",1000);
-    sendData("AT+CIFSR\r\n",1000); // get ip address
-    sendData("AT+CIPMUX=1\r\n",1000); // configure for multiple connections
-    sendData("AT+CIPSERVER=1,80\r\n",1000); // turn on server on port 80
+  Serial.begin(9600);
+  esp8266.begin(9600); // your esp's baud rate might be different
+  
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+   
+  sendCommand("AT+CWMODE=1\r\n",1000,DEBUG); // configure as access point
+  sendCommand("AT+CWJAP=\"P700\",\"SousChefPasteK\"\r\n",3000,DEBUG);
+  delay(10000);
+  sendCommand("AT+CIFSR\r\n",1000,DEBUG); // get ip address
+  sendCommand("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
+  sendCommand("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // turn on server on port 80
+  
+  Serial.println("Server Ready");
 }
 
 void loop()
 {
-    if(esp.available())
+  if(esp8266.available()) // check if the esp is sending a message 
+  {
+
+    
+    if(esp8266.find("+IPD,"))
     {
-        if(esp.find("+IPD,"))
-        {
-            delay(1000);;
-            int connectionId = esp.read()-48;//substract 48 because '0' char is at 48th position in ASCII table
-            
-            esp.find("red=");
-            int R = ConvertHexaToLum(esp.read(),esp.read());
+     delay(1000); // wait for the serial buffer to fill up (read all the serial data)
+     // get the connection id so that we can then disconnect
+     int connectionId = esp8266.read()-48; // subtract 48 because the read() function returns 
+                                           // the ASCII decimal value and 0 (the first decimal number) starts at 48
+          
+     esp8266.find("red="); // advance cursor to "pin="
+          
+     int high = (esp8266.read()); // get first number i.e. if the pin 13 then the 1st number is 1
+     int low = (esp8266.read());
+     int R = ConvertHexaToLum(high, low);
+     analogWrite(REDPIN, R);
 
-            esp.find("green=");
-            int G = ConvertHexaToLum(esp.read(),esp.read());
+     esp8266.find("green="); // advance cursor to "pin="
+     high = (esp8266.read()); // get first number i.e. if the pin 13 then the 1st number is 1
+     low = (esp8266.read());
+     int G = ConvertHexaToLum(high, low);
+     analogWrite(GREENPIN, G);
 
-            esp.find("blue=");
-            int B = ConvertHexaToLum(espread(),esp.read());
-            Serial.println(G);
-
-            // make close command
-            String closeCommand = "AT+CIPCLOSE="; 
-            closeCommand+=connectionId; // append connection id
-            closeCommand+="\r\n";
-            
-            sendData(closeCommand,1000,DEBUG); // close connection
-        }
+     esp8266.find("blue="); // advance cursor to "pin="
+     high = (esp8266.read()); // get first number i.e. if the pin 13 then the 1st number is 1
+     low = (esp8266.read());
+     int B = ConvertHexaToLum(high, low);
+     analogWrite(BLUEPIN, B);
+     
+     // build string that is send back to device that is requesting pin toggle
+     String content;
+     content = "Color set";
+     content += R;
+     content += G;
+     content += B;
+     content += high;
+     content += low;
+     
+     sendHTTPResponse(connectionId,content);
+     
+     // make close command
+     String closeCommand = "AT+CIPCLOSE="; 
+     closeCommand+=connectionId; // append connection id
+     closeCommand+="\r\n";
+     
+     sendCommand(closeCommand,1000,DEBUG); // close connection
     }
+  }
 }
 
-int ConvertHexaToLum(int high, int low)
-{
-    int value = 0;
-    if(high < 65)//value between 0 & 9
-    {
-        value += (high-48)*16;
-    }
-    else
-    {
-        value += (high-65)*16;
-    }
-    if(low < 65)
-    {
-        value += (low-48);
-    }
-    else
-    {
-        value += (low-65);
-    }
-}
-
-
+/*
+* Name: sendData
+* Description: Function used to send data to ESP8266.
+* Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
+* Returns: The response from the esp8266 (if there is a reponse)
+*/
 String sendData(String command, const int timeout, boolean debug)
 {
     String response = "";
     
+    int dataSize = command.length();
+    char data[dataSize];
+    command.toCharArray(data,dataSize);
+           
+    esp8266.write(data,dataSize); // send the read character to the esp8266
+    if(debug)
+    {
+      Serial.println("\r\n====== HTTP Response From Arduino ======");
+      Serial.write(data,dataSize);
+      Serial.println("\r\n========================================");
+    }
+    
+    long int time = millis();
+    
+    while( (time+timeout) > millis())
+    {
+      while(esp8266.available())
+      {
+        
+        // The esp has data so display its output to the serial window 
+        char c = esp8266.read(); // read the next character.
+        response+=c;
+      }  
+    }
+    
+    if(debug)
+    {
+      Serial.print(response);
+    }
+    
+    return response;
+}
+
+/*
+* Name: sendHTTPResponse
+* Description: Function that sends HTTP 200, HTML UTF-8 response
+*/
+void sendHTTPResponse(int connectionId, String content)
+{
+     
+     // build HTTP response
+     String httpResponse;
+     String httpHeader;
+     // HTTP Header
+     httpHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n"; 
+     httpHeader += "Content-Length: ";
+     httpHeader += content.length();
+     httpHeader += "\r\n";
+     httpHeader +="Connection: close\r\n\r\n";
+     httpResponse = httpHeader + content + " "; // There is a bug in this code: the last character of "content" is not sent, I cheated by adding this extra space
+     sendCIPData(connectionId,httpResponse);
+}
+/*
+* Name: sendCIPDATA
+* Description: sends a CIPSEND=<connectionId>,<data> command
+*
+*/
+void sendCIPData(int connectionId, String data)
+{
+   String cipSend = "AT+CIPSEND=";
+   cipSend += connectionId;
+   cipSend += ",";
+   cipSend +=data.length();
+   cipSend +="\r\n";
+   sendCommand(cipSend,1000,DEBUG);
+   sendData(data,1000,DEBUG);
+}
+/*
+* Name: sendCommand
+* Description: Function used to send data to ESP8266.
+* Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
+* Returns: The response from the esp8266 (if there is a reponse)
+*/
+String sendCommand(String command, const int timeout, boolean debug)
+{
+    String response = "";
+           
     esp8266.print(command); // send the read character to the esp8266
     
     long int time = millis();
@@ -92,4 +189,26 @@ String sendData(String command, const int timeout, boolean debug)
     }
     
     return response;
+}
+
+int ConvertHexaToLum(int high, int low)
+{
+    int value = 0;
+    if(high < 65)//value between 0 & 9
+    {
+        value += (high-48)*16;
+    }
+    else
+    {
+        value += (high-55)*16;
+    }
+    if(low < 65)
+    {
+        value += (low-48);
+    }
+    else
+    {
+        value += (low-55);
+    }
+    return value;
 }
